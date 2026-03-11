fc2d6369f4d0c8d615bee9f105c6705ac9fe9a5a
Samuel Solano <sdsolano@uninorte.edu.co>
2026-03-11 14:24:19 -0500

    Api url change

--- src/prediction/festivos_api.py
+++ src/prediction/festivos_api.py
@@ -23,7 +23,7 @@ class FestivosAPIClient:
     (ForecastPipeline, CalendarClassifier) durante la ejecución actual.
     """
     
-    BASE_URL = "https://pronosticos.jmdatalabs.co/api/v1/admin/dashboard/configuracion/listarFestivos"
+    BASE_URL = "https://pronosticos.jmdatalabs.co/api/v1/admin/configuracion-interna/listarFestivos"
     
     def __init__(self, base_url: Optional[str] = None):
         """
